9766f1fe7455fada1a5ac168cca4c6a1b38fc2cc
1655986984 <1655986984@qq.com>
2024-04-25 10:20:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,3 +19,9 @@ sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 #修改wifi名称（mtwifi-cfg）
 sed -i 's/ImmortalWrt-2.4G/CMCC_2.4G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i 's/ImmortalWrt-5G/CMCC_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+#wifi加密方式，没有是none
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#wifi密码
+sed -i 's/key=15581822425/key=1234567890/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
